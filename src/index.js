1025789a0a0cc773e61ea2aa0da26e96d86190d1
user <raheemadebayorr@gmail.con>
2023-10-17 13:21:37 +0100

    practice fetching data in Firestore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app"
 
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "firebase/firestore"
+
 const firebaseConfig = {
     apiKey: "AIzaSyA8SZo0dwbo2suY_aiL9E0WLa243BXoIYU",
     authDomain: "fir-9-tutorial-3fc47.firebaseapp.com",
@@ -10,3 +16,17 @@ const firebaseConfig = {
   };
 
 initializeApp(firebaseConfig)
+
+const db = getFirestore() 
+
+const colRef = collection(db, "Books")
+
+getDocs(colRef)
+.then((snapshot)=> {
+  console.log(snapshot)
+  let books = []
+  snapshot.docs.forEach((doc) => {
+  books.push({...doc.data(), id: doc.id})
+  console.log(books)
+  })
+})
