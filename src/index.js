f1416fba372cedbc5c0abe53085f21eace4ffbd9
user <raheemadebayorr@gmail.con>
2023-10-17 13:33:58 +0100

    set up the structure of the app and required node modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8SZo0dwbo2suY_aiL9E0WLa243BXoIYU",
+    authDomain: "fir-9-tutorial-3fc47.firebaseapp.com",
+    projectId: "fir-9-tutorial-3fc47",
+    storageBucket: "fir-9-tutorial-3fc47.appspot.com",
+    messagingSenderId: "413992773551",
+    appId: "1:413992773551:web:e08563f704024945c1715a"
+  };
+
+initializeApp(firebaseConfig)
